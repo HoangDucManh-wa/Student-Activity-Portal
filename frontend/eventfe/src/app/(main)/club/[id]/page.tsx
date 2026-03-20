efb22c12e2f87271e33a44f5b4fa078eb5a7fd08
KhangHKRNguyen <7zbbkingz7@gmail.com>
2026-03-20 19:11:36 +0700

    Remove the contact section

--- frontend/eventfe/src/app/(main)/club/[id]/page.tsx
+++ frontend/eventfe/src/app/(main)/club/[id]/page.tsx
@@ -55,21 +55,6 @@ export default async function DetailClubPage({
       <div className="mx-[20px] rounded-[20px] border-[3px] border-[#1A73E8] p-[20px] h-[600px]">
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum facilis, vel voluptatibus possimus esse quidem est repellat sequi quasi, assumenda dolor, aspernatur laudantium quam voluptatum nisi numquam velit odio? Facilis!
       </div>
-      <div className="flex items-center w-full my-10">
-        <div className="flex-1 h-[3px] bg-[#08667a]"></div>
-        <div className="px-0">
-          <span className="bg-[#08667a] text-white px-8 py-2 rounded-full font-bold text-[16px] whitespace-nowrap uppercase tracking-wider">
-            Liên hệ
-          </span>
-        </div>
-        <div className="flex-1 h-[3px] bg-[#08667a]"></div>
-      </div>
-      <div className="mx-[20px] rounded-[12px] border-[3px] border-[#1A73E8] p-[20px] h-[200px] mb-[30px] space-y-[15px]">
-        <div className="text-[16px] font-bold">Tiktok: </div>
-        <div className="text-[16px] font-bold">Email: </div>
-        <div className="text-[16px] font-bold">Facebook: </div>
-        <div className="text-[16px] font-bold">Số điện thoại: </div>
-      </div>
       <h1 className="mb-[30px] text-[24px] font-bold text-center text-[#1A73E8]">CÂU LẠC BỘ HIỆN KHÔNG MỞ ĐƠN TUYỂN THÀNH VIÊN!</h1>
       <div className="flex justify-center mt-4">
       <Link
